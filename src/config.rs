5bb85f561ca8a47a9b51560dc0a42f0dd2257742
Ben <ben@closelink.com>
2024-07-26 10:28:44 +0200

    add error message if config is not found

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -11,8 +11,11 @@ pub struct GlobalConfig {
 impl GlobalConfig {
     pub fn read() -> Result<GlobalConfig> {
         let os_home = std::env::var("HOME")?;
-        let file_content = fs::read_to_string(format!("{os_home}/.gstack/config.toml"))?;
-        let config: GlobalConfig = toml::from_str(file_content.as_str())?;
-        Ok(config)
+        if let Ok(file_content) = fs::read_to_string(format!("{os_home}/.gstack/config.toml")) {
+            let config: GlobalConfig = toml::from_str(file_content.as_str())?;
+            Ok(config)
+        } else {
+            bail!("Could not find g-stack config at default location $HOME/.gstack/config.toml")
+        }
     }
 }
